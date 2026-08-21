438fef5874dae328af9b8104e3c40ed3341e9723
Felipe Alvarado <devfelipe663@gmail.com>
2026-08-20 19:44:41 -0500

    add files and setup project server to management the movies

---
+++ backend/src/main/java/BackMoviesManager/BackMoviesApplication.java
@@ -0,0 +1,13 @@
+package BackMoviesManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackMoviesApplication.class, args);
+		System.out.println("BackMoviesApplication started successfully!");
+	}
+}
---
+++ backend/src/test/java/BackMoviesManager/BackMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package BackMoviesManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
